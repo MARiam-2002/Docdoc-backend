cf8cfd2ea3eb71a4d99c7ce128c75d72b22a827f
mariam khaled <khaldmrym769@gmail.com>
2024-09-21 00:16:05 +0300

    update

--- src/modules/auth/controller/auth.js
+++ src/modules/auth/controller/auth.js
@@ -179,10 +179,9 @@ export const logout = asyncHandler(async (req, res, next) => {
 });
 
 export const updateProfile = asyncHandler(async (req, res, next) => {
-  const user = await findOne({
-    _id: req.user._id, // Assuming the user ID is stored in the request object
-  });
-
+  const user = await userModel.findById(
+    req.user._id // Assuming the user ID is stored in the request object
+  );
 
   // Assuming the request body contains the fields to update
   const { name, birthDay, email, country, phone } = req.body;
@@ -225,7 +224,7 @@ export const updateProfile = asyncHandler(async (req, res, next) => {
   }
 
   // Update user fields
-  user.name = name?name:user.name;
+  user.name = name ? name : user.name;
   user.birthDay = birthDay ? new Date(birthDay) : user.birthDay;
   user.email = email ? email : user.email;
   user.country = country ? country : user.country;
