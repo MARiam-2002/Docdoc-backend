7d03fa49bc8eb543e78f781264fa6fdde5eca1da
mariam khaled <khaldmrym769@gmail.com>
2024-09-24 17:44:56 +0300

    Add governorates with cities Api

--- src/modules/cityWithGov/controller/cityWithGov.js
+++ src/modules/cityWithGov/controller/cityWithGov.js
@@ -1,20 +1,31 @@
-
 import cityModel from "../../../../DB/models/city.model.js";
 import governorateModel from "../../../../DB/models/governorates.model.js";
 import { asyncHandler } from "../../../utils/asyncHandler.js";
 
-
 export const getAllCityWithGov = asyncHandler(async (req, res, next) => {
-  const governorate = await cityModel
-    .find({
-      governorateId:"66f2618089b3c3692b849dc3"
-
+  const governorates = await governorateModel
+    .find({})
+    .select("name _id location")
+    .populate({
+      path: "cities", // جلب المدن المرتبطة
+      select: "name _id location -governorateId", // جلب الحقول المطلوبة فقط
     })
-     
-  return res.status(200).json({
-    success: true,
-    data: governorate,
+    .lean(); // استخدام `lean` لجعل الاستعلام أسرع وإعادة كائنات JavaScript عادية
 
+  // التحقق من وجود البيانات
+  if (!governorates || governorates.length === 0) {
+    return res.status(404).json({
+      message: "No data found",
+      status: false,
+      code: 404,
+    });
+  }
+
+  // إرسال الاستجابة بشكل صحيح مع المدن المرتبطة
+  return res.status(200).json({
+    message: "Successful query",
+    data: governorates, // جلب المحافظات مع المدن المرتبطة
+    status: true,
+    code: 200,
   });
 });
-
