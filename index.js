1f46932f26cff09f96ef5ab1fdc4793f61073e03
mariam khaled <khaldmrym769@gmail.com>
2024-09-29 05:38:37 +0300

    update

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const app = express();
 const port = process.env.PORT;
 
 connectDB();
+app.use('/.well-known', express.static('.well-known'));
+
 bootstrap(app, express);
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
